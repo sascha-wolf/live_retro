05757cd255b4d2bd04f702de94b067c490d34738
Sascha Wolf <swolf.dev@gmail.com>
2019-06-07 11:17:43 +0200

    Live - Cards: Split the rendering into multiple functions

--- lib/live_retro_web/live/cards_live.ex
+++ lib/live_retro_web/live/cards_live.ex
@@ -1,41 +1,56 @@
 defmodule LiveRetroWeb.CardsLive do
   use Phoenix.LiveView
 
+  @max_grid_size 12
+  @min_column_size 2
+
   def render(%{columns: columns} = assigns) do
-    column_size = calculate_column_size(columns)
+    column_size =
+      @max_grid_size
+      |> div(length(columns))
+      |> max(@min_column_size)
 
     ~L"""
     <div class="row">
       <%= for column <- @columns do %>
-        <section>
-          <div class="col s<%= column_size %>">
-            <div class="row">
-              <div class="col s8">
-                <h5><%= column[:title] %></h5>
-
-              </div>
-            </div>
-
-            <%= for card <- column[:cards] do %>
-              <item>
-                <div class="card blue-grey darken-1">
-                  <div class="card-content white-text">
-                    <%= card %>
-                  </div>
-                </div>
-              </item>
-            <% end %>
-          </div>
-        </section>
+        <%=
+          column
+          |> Map.put(:column_size, column_size)
+          |> render_column()
+        %>
       <% end %>
     </div>
     """
   end
 
-  defp calculate_column_size(columns) do
-    12
-    |> div(length(columns))
-    |> max(2)
+  defp render_column(assigns) do
+    ~L"""
+    <section>
+      <div class="col s<%= @column_size %>">
+        <div class="row">
+          <div class="col s8">
+            <h5><%= @title %></h5>
+          </div>
+        </div>
+
+        <%= for card <- @cards do %>
+          <%= render_card(card) %>
+        <% end %>
+      </div>
+    </section>
+    """
+  end
+
+  defp render_card(assigns) do
+    ~L"""
+    <item>
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <%= @text %>
+        </div>
+      </div>
+    </item>
+    """
   end
 
   def mount(_session, socket) do
@@ -43,19 +58,19 @@ defmodule LiveRetroWeb.CardsLive do
       %{
         title: "First Row!",
         cards: [
-          "Hello, I am a card!"
+          %{text: "Hello, I am a card!"}
         ]
       },
       %{
         title: "Second Row!",
         cards: [
-          "Hello, I am a card!"
+          %{text: "Hello, I am a card!"}
         ]
       },
       %{
         title: "Third Row!",
         cards: [
-          "Hello, I am a card!"
+          %{text: "Hello, I am a card!"}
         ]
       }
     ]
