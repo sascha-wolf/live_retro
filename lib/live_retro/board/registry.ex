9ff4ff1345e3ba7b4c446a66eeb1a1ee3f1a41a2
Sascha Wolf <swolf.dev@gmail.com>
2019-06-07 23:20:29 +0200

    Board: Use a DynamicSupervisor in conjunction with a Registry for boards

--- lib/live_retro/board/registry.ex
+++ lib/live_retro/board/registry.ex
@@ -1,32 +1,39 @@
 defmodule LiveRetro.Board.Registry do
-  @registry __MODULE__
+  use Supervisor
 
-  def child_spec([]) do
-    %{
-      id: @registry,
-      start: {__MODULE__, :start_link, []},
-      type: :supervisor
-    }
+  alias LiveRetro.Board.Storage, as: Board
+
+  @registry __MODULE__
+  @supervisor __MODULE__.Supervisor
+
+  def start_link(_) do
+    Supervisor.start_link(__MODULE__, [])
   end
 
-  def start_link do
-    Registry.start_link(keys: :unique, name: @registry)
+  @impl true
+  def init(_) do
+    children = [
+      {DynamicSupervisor, strategy: :one_for_one, name: @supervisor},
+      {Registry, keys: :unique, name: @registry}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_all)
   end
 
   def new(board) do
-    {:ok, pid} = LiveRetro.Board.Storage.start_link(name_for(board))
-  end
-
-  def lookup(board) do
     name = name_for(board)
 
+    DynamicSupervisor.start_child(@supervisor, {Board, name})
+  end
+
+  def name_for(board), do: {:via, Registry, {@registry, board}}
+
+  def lookup(board) do
     @registry
-    |> Registry.lookup(name)
+    |> Registry.lookup(board)
     |> case do
       [{pid, _}] -> {:ok, pid}
       [] -> {:error, :not_found}
     end
   end
-
-  def name_for(board), do: {:via, Registry, {@registry, board}}
 end
