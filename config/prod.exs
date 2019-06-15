75faa99f8048979a46bc42e4d59deac3de7b751c
Sascha Wolf <swolf.dev@gmail.com>
2019-06-15 22:31:19 +0200

    Prod: Prep for deployment on Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,12 @@
 use Mix.Config
 
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
 # For production, don't forget to configure the url host
 # to something meaningful, Phoenix uses this information
 # when generating URLs.
@@ -10,8 +17,14 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :live_retro, LiveRetroWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [
+    scheme: "https",
+    host: "live-retro",
+    port: 80
+  ],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: secret_key_base
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -62,4 +75,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
